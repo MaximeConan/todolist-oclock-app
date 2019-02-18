d31fecf71ba21f796c1d626c49a5a130a9a7338c
Maxime Conan <maximeconan5@gmail.com>
2019-02-18 11:56:20 +0100

    dynamisation de <Counter />

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -11,13 +11,28 @@ import Tasks from 'src/components/Tasks'
 import taskList from 'src/datas/tasks'
 
 // Code
-const App = () => (
-  <div id="app">
-    <Form />
-    <Counter count={3} />
-    <Tasks list={taskList} />
-  </div>
-)
+class App extends React.Component {
+  // Définition du state
+  state = {
+    tasks: taskList,
+  }
+
+  render() {
+    // Récup des tâches dans le state
+    const { tasks } = this.state
+
+    // Récup du nombre de tâches en cours
+    const count = tasks.filter(task => !task.done).length
+
+    return (
+      <div id="app">
+        <Form />
+        <Counter count={count} />
+        <Tasks list={tasks} />
+      </div>
+    )
+  }
+}
 
 // Export
 export default App
